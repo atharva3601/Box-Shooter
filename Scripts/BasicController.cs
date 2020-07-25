814b03a56877ae2fb0ed9477564287e4943e23eb
atharva3601 <66211622+atharva3601@users.noreply.github.com>
2020-07-25 20:34:05 +0530

    Add files via upload

---
+++ Scripts/BasicController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour
+{
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log ("Horizontal Input = "+Input.GetAxis ("Horizontal"));
+    }
+}
